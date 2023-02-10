6b3316479baa6a36af3c9b3e71a9af4707c57805
Erik van Velzen <erik@evanv.nl>
2023-02-11 00:17:49 +0100

    sqlgen: split dialect file
    Organize runtime dialect functions into one source file per dialect
    for better navigability. In preparation to add another function.
    
    Change-Id: I3b341f2b96cf9ad4d9441e2eed0987e6b6465fc4

---
+++ sqlgen/doc.go
@@ -0,0 +1,7 @@
+// Copyright (C) 2023 Storj Labs, Inc.
+
+/*
+Package sqlgen contains code which is included as-is in the final template.
+Package sqlgen/sqlbundle joins all the code in package sqlgen to a single string.
+*/
+package sqlgen
